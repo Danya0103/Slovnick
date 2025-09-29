9df3da9bdd8415b9ca4901ccd8931a6e7a48f2de
Danya0103 <user@WIN-LRBB7CDB9IA>
2025-09-29 14:49:58 +0300

    Добавьте файлы проекта.

---
+++ Slovnick/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Slovnick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtTranslate = new System.Windows.Forms.TextBox();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.txtTranslate);
+            this.panel1.Controls.Add(this.txtWord);
+            this.panel1.Location = new System.Drawing.Point(132, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(557, 385);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSearch.Location = new System.Drawing.Point(309, 311);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Пошук";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(82, 312);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(212, 22);
+            this.txtSearch.TabIndex = 7;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLoad.Location = new System.Drawing.Point(368, 211);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(119, 23);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Завантажити";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Location = new System.Drawing.Point(368, 182);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(119, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Зберегти";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDelete.Location = new System.Drawing.Point(368, 151);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(119, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Видалити";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(82, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 180);
+            this.listBox1.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Location = new System.Drawing.Point(368, 41);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Додати";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtTranslate
+            // 
+            this.txtTranslate.Location = new System.Drawing.Point(188, 43);
+            this.txtTranslate.Name = "txtTranslate";
+            this.txtTranslate.Size = new System.Drawing.Size(163, 22);
+            this.txtTranslate.TabIndex = 1;
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(82, 43);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(100, 22);
+            this.txtWord.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 452);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtTranslate;
+        private System.Windows.Forms.TextBox txtWord;
+    }
+}
+
---
+++ Slovnick/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slovnick
+{
+
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> slovnick = new Dictionary<string, string>();
+        /*
+         * { "apple" : "yabloko" ; "orange" :"appelsin";
+         * 
+         */
+        private string filePath = "slovnick.txt"; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // .Add() - lодати в словник
+            // slovncik[key] = value; додати/оновити
+
+            string word = txtWord.Text.Trim();
+            string translate = txtTranslate.Text;
+
+            if (!string.IsNullOrEmpty(word) && !string.IsNullOrEmpty(translate))
+            {
+                slovnick[word] = translate;
+                txtWord.Clear();
+                txtTranslate.Clear();
+                UpdateList();
+            }
+        }
+
+        private void UpdateList()
+        {
+            listBox1.Items.Clear();
+
+            foreach(var pair in slovnick)
+            {
+                listBox1.Items.Add($"{pair.Key} - {pair.Value}"); // big-O
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                // asdasd - dasdasda
+                // after Split : ["asdasd ", " dasdasda"]
+
+                string word = listBox1.SelectedItem.ToString().Split('-')[0].Trim();
+                //string translate = listBox1.SelectedItem.ToString().Split('-')[1].Trim();
+
+                slovnick.Remove(word);
+                UpdateList();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchSlovo = txtSearch.Text.ToLower().Trim();
+
+            if (searchSlovo != "")
+            {
+                listBox1.Items.Clear();
+
+                foreach (var pair in slovnick)
+                {
+                    if (pair.Key.ToLower().StartsWith(searchSlovo))
+                    {
+                        listBox1.Items.Add($"{pair.Key} - {pair.Value}");
+                    }
+                }
+
+                if (listBox1.Items.Count == 0)
+                {
+                    MessageBox.Show("404");
+                    UpdateList();
+                }
+            }
+
+            else
+            {
+                UpdateList();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach(var pair in slovnick)
+                {
+                    sw.WriteLine($"{pair.Key}: {pair.Value}");
+                }
+            }
+            MessageBox.Show("Файл успішно збережено");
+        }
+    }
+}
